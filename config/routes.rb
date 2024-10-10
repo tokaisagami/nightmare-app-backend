9dd94410e647723d804e0f1fccfc293ac2a6a5b1
tokaisagami <tokai.sagami.54@gmail.com>
2024-10-10 22:08:25 +0900

    update:仮のトップページ用のAPI作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:index, :show, :create, :update, :destroy]
+      resources :placeholders, only: [:index]
     end
   end
-
-  # 他のルートを定義する場合も同様に
-  # namespace :api do
-  #   namespace :v1 do
-  #     resources :articles, only: [:index, :show, :create, :update, :destroy]
-  #   end
-  # end
 end
