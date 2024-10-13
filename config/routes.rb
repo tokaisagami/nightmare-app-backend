777d1760c6487c0036aded80682865b6ff8a6080
tokaisagami <tokai.sagami.54@gmail.com>
2024-10-13 23:35:41 +0900

    update:ルーティングにログイン用のルートを設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :placeholders, only: [:index]
       resources :users, only: [:create]
+      post 'login', to: 'user_sessions#create'
     end
   end
 end
