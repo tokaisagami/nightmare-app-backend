f59e7950b534f27732bafa0b268204735f3e7ff8
tokaisagami <tokai.sagami.54@gmail.com>
2024-10-19 22:31:02 +0900

    update:ログインエンドポイントの設定

--- app/controllers/api/v1/user_sessions_controller.rb
+++ app/controllers/api/v1/user_sessions_controller.rb
@@ -6,7 +6,8 @@ module Api
       def create
         user = User.find_by(email: params[:email])
         if user && user.authenticate(params[:password])
-          render json: { message: 'Login successful', user: user }, status: :ok
+          token = JWT.encode({ user_id: user.id }, Rails.application.credentials.secret_key_base)
+          render json: { message: 'Login successful', token: token }, status: :created # トークンを返す
         else
           render json: { error: 'Invalid email or password' }, status: :unauthorized
         end
