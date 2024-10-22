e5b0b75e4ed51bc455c4edafde28a31955215d9a
tokaisagami <tokai.sagami.54@gmail.com>
2024-10-22 23:38:31 +0900

    add:Nightmaresコントローラに対応するルートを設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
       resources :placeholders, only: [:index]
       resources :users, only: [:create]
       post 'login', to: 'user_sessions#create'
+      resources :nightmares, only: [:index, :show, :create, :update, :destroy]
     end
   end
 end
