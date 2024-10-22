71f9aab61ba4f78df310bb48358ef8fae9643a42
tokaisagami <tokai.sagami.54@gmail.com>
2024-10-22 23:37:52 +0900

    add:バリデーションを追加

--- app/models/nightmare.rb
+++ app/models/nightmare.rb
@@ -1,3 +1,8 @@
 class Nightmare < ApplicationRecord
   belongs_to :user
+
+  validates :description, presence: true
+  validates :modified_description, presence: true
+  validates :ending_type, presence: true
+  validates :published, inclusion: { in: [true, false] }
 end
