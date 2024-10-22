891719837d2d1fe0148b31b58e9b876d58cf5a1f
tokaisagami <tokai.sagami.54@gmail.com>
2024-10-22 21:48:08 +0900

    add:検証追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,11 +12,11 @@ class ApplicationController < ActionController::API
   def authenticate_token
     authenticate_with_http_token do |token, options|
       puts "Received token: #{token}" # トークンをログに出力
-      puts "シークレットキー開始" # トークンをログに出力
       secret_key = Rails.application.credentials.secret_key_base || ENV['SECRET_KEY_BASE']
-      puts "シークレットキー成功" # トークンをログに出力
       begin
+        puts "ペイロードとヘッダー取得開始" # トークンをログに出力
         payload, header = JWT.decode(token, Rails.application.credentials.secret_key_base)
+        puts "ペイロードとヘッダー取得成功" # トークンをログに出力
         puts "Payload: #{payload.inspect}"
         puts "Header: #{header.inspect}"
         @current_user = User.find_by(id: payload['user_id'])
