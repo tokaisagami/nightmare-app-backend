5f75eb80d3704cd03015f0113726138e4932259b
tokaisagami <tokai.sagami.54@gmail.com>
2024-10-22 19:33:17 +0900

    Fix JWT authentication and update database configuration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,11 +11,20 @@ class ApplicationController < ActionController::API
 
   def authenticate_token
     authenticate_with_http_token do |token, options|
-      puts "Received token: #{token}" # トークンをログに出力
+      puts "Received token: #{token}"
+      secret_key = Rails.application.credentials.secret_key_base || ENV['SECRET_KEY_BASE']
       begin
-        payload, header = JWT.decode(token, Rails.application.credentials.secret_key_base)
-        @current_user = User.find_by(id: payload['user_id'])
-      rescue JWT::DecodeError
+        payload, header = JWT.decode(token, secret_key)
+        puts "Payload: #{payload}" # ペイロードの内容をログに出力
+        user_id = payload['user_id']
+        @current_user = User.find_by(id: user_id)
+        if @current_user
+          puts "Current User: #{@current_user.inspect}" # ユーザーをログに出力
+        else
+          puts "User not found for ID: #{user_id}" # ユーザーが見つからない場合
+        end
+      rescue JWT::DecodeError => e
+        puts "JWT Decode Error: #{e.message}" # デコードエラーをログに出力
         nil
       end
     end
