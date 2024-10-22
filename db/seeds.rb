93d3df3087994e166d18ceca9dcb550591b07051
tokaisagami <tokai.sagami.54@gmail.com>
2024-10-22 23:37:22 +0900

    add:仮のデータをNightmaresテーブルに挿入

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(email: 'user1@example.com', password: 'password')
+User.create(email: 'user2@example.com', password: 'password')
+
+Nightmare.create(user_id: 1, description: '悪夢の説明１', modified_description: '改変後の説明１', ending_type: 'ハッピーエンド', published: true)
+Nightmare.create(user_id: 2, description: '悪夢の説明２', modified_description: '改変後の説明２', ending_type: 'バッドエンド', published: false)
