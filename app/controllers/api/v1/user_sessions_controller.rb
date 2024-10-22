fe3091ed81366d34180f590dc4881f385d866552
tokaisagami <tokai.sagami.54@gmail.com>
2024-10-22 21:37:08 +0900

    fix:トークン生成修正

--- app/controllers/api/v1/user_sessions_controller.rb
+++ app/controllers/api/v1/user_sessions_controller.rb
@@ -6,7 +6,7 @@ module Api
       def create
         user = User.find_by(email: params[:email])
         if user && user.authenticate(params[:password])
-          token = JWT.encode({ user_id: user.id }, Rails.application.credentials.secret_key_base)
+          token = JWT.encode({ user_id: user.id, exp: 24.hours.from_now.to_i }, Rails.application.credentials.secret_key_base, 'HS256')
           render json: { message: 'Login successful', token: token }, status: :created # トークンを返す
         else
           render json: { error: 'Invalid email or password' }, status: :unauthorized
