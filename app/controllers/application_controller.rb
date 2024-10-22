38642e84b83b6611b5022dac14c20269b624a963
tokaisagami <tokai.sagami.54@gmail.com>
2024-10-22 21:51:41 +0900

    update:ログ詳細確認追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,13 +15,20 @@ class ApplicationController < ActionController::API
       secret_key = Rails.application.credentials.secret_key_base || ENV['SECRET_KEY_BASE']
       begin
         puts "ペイロードとヘッダー取得開始" # トークンをログに出力
-        payload, header = JWT.decode(token, Rails.application.credentials.secret_key_base)
+        payload, header = JWT.decode(token, secret_key, true, { algorithm: 'HS256' }) # アルゴリズムの指定を追加
         puts "ペイロードとヘッダー取得成功" # トークンをログに出力
         puts "Payload: #{payload.inspect}"
         puts "Header: #{header.inspect}"
         @current_user = User.find_by(id: payload['user_id'])
-      rescue JWT::DecodeError
-        nil
+      rescue JWT::ExpiredSignature
+        puts "Token has expired"
+        render json: { error: 'Token has expired' }, status: :unauthorized
+      rescue JWT::VerificationError => e
+        puts "JWT Verification Error: #{e.message}" # JWTの検証エラーをキャッチ
+        render json: { error: 'JWT Verification Error' }, status: :unauthorized
+      rescue JWT::DecodeError => e
+        puts "JWT Decode Error: #{e.message}" # JWTのデコードエラーをキャッチ
+        render json: { error: 'Invalid token' }, status: :unauthorized
       end
     end
   end
