a33d8ed3fd561dd63f7490cf9f3a3d80f8033ac7
tokaisagami <tokai.sagami.54@gmail.com>
2024-10-24 00:59:06 +0900

    update:項目変更対応_schema反映（ending_type⇒ending_category)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_10_22_140033) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_23_153742) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,7 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_22_140033) do
     t.integer "user_id"
     t.text "description"
     t.text "modified_description"
-    t.string "ending_type"
+    t.integer "ending_category", default: 0, null: false
     t.boolean "published"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
