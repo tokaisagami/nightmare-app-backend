721044667d59009459a632c4a011b63612e6bd93
tokaisagami <tokai.sagami.54@gmail.com>
2024-10-24 00:58:33 +0900

    update:項目変更対応（ending_type⇒ending_category)

--- app/models/nightmare.rb
+++ app/models/nightmare.rb
@@ -1,8 +1,10 @@
 class Nightmare < ApplicationRecord
   belongs_to :user
 
+  enum ending_category: { happy_end: 0, unexpected_end: 1, bad_end: 2 }
+
   validates :description, presence: true
   validates :modified_description, presence: true
-  validates :ending_type, presence: true
+  validates :ending_category, presence: true
   validates :published, inclusion: { in: [true, false] }
 end
