9adf69b83973550cdeb47da48ff48e464c0f4f01
tokaisagami <tokai.sagami.54@gmail.com>
2024-10-26 18:00:39 +0900

    update:プロンプトの内容をさらに修正

--- app/services/chat_gpt_service.rb
+++ app/services/chat_gpt_service.rb
@@ -3,13 +3,14 @@ require 'openai'
 class ChatGptService
   def self.modify_nightmare(nightmare, ending_type)
     client = OpenAI::Client.new(api_key: ENV['OPENAI_API_KEY'])
-    model = 'gpt-4o' # もしくは 'gpt-4o-mini'
+    model = 'gpt-4o'
+#    model = 'gpt-4o-mini'
 
     prompt = case ending_type
              when 0
                "あなたは優秀な物語のライターです。\n次の悪夢の内容をはじめからまるごとハッピーエンドに改変してください。\n悪夢の内容：#{nightmare}"
              when 1
-               "あなたはボボボーボ・ボーボボの原作者と同じ思考回路を持ったAIです。\n次の悪夢の内容をはじめからまるごと予想外の内容に改変してください。\nただし、ボボボーボ・ボーボボの登場キャラは出さずにお願いします。\n悪夢の内容：#{nightmare}"
+               "あなたはボボボーボ・ボーボボの原作者と同じ思考回路を持ったAIです。\n次の悪夢の内容をボボボーボ・ボーボボの展開のようにめちゃくちゃな内容にはじめからまるごと改変してください。\nただし、ボボボーボ・ボーボボの登場キャラは出さずにお願いします。\n悪夢の内容：#{nightmare}"
              else
                "以下の悪夢の内容を改変してください。\n悪夢の内容：#{nightmare}"
              end
