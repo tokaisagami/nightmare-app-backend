5b56026f4f9c67723ef892a16d2fbc6ec73de84b
tokaisagami <tokai.sagami.54@gmail.com>
2025-01-17 21:45:58 +0900

    add:パスワードリセットルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :placeholders, only: [:index]
       resources :users, only: [:create, :show, :update] # :updateアクションを追加
+      resources :password_resets, only: [:create, :edit, :update]
       post 'login', to: 'user_sessions#create'
       resources :nightmares, only: [:index, :show, :create, :update, :destroy] do
         collection do
