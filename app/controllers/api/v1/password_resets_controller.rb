5816ad7a538a1dd1fba60ddaf807250a6d75b5cb
tokaisagami <tokai.sagami.54@gmail.com>
2025-01-25 23:02:57 +0900

    add:password_resets_controller追加

--- app/controllers/api/v1/password_resets_controller.rb
+++ app/controllers/api/v1/password_resets_controller.rb
@@ -1,31 +1,40 @@
 module Api
   module V1
     class PasswordResetsController < ApplicationController
+      skip_before_action :require_login, only: [:create, :edit, :update]
+
       def create
-        user = User.find_by(email: params[:email])
-        if user
-          user.deliver_reset_password_instructions!
+        @user = User.find_by_email(params[:email])
+        if @user
+          @user.deliver_reset_password_instructions!
+          render json: { message: 'Instructions have been sent to your email.' }, status: :ok
+        else
+          render json: { error: 'Email address not found.' }, status: :not_found
         end
-        render json: { message: "パスワードリセット手順を送信しました" }, status: :ok
       end
 
       def edit
+        @token = params[:id]
         @user = User.load_from_reset_password_token(params[:id])
-        if @user.blank?
-          not_authenticated
+
+        unless @user
+          render json: { error: "Invalid reset token" }, status: :unauthorized
           return
         end
       end
 
       def update
+        @token = params[:id]
         @user = User.load_from_reset_password_token(params[:id])
-        if @user.blank?
-          not_authenticated
+
+        unless @user
+          render json: { error: "Invalid reset token" }, status: :unauthorized
           return
         end
 
-        if @user.change_password(params[:password])
-          render json: { message: "パスワードをリセットしました" }, status: :ok
+        @user.password_confirmation = params[:user][:password_confirmation]
+        if @user.change_password(params[:user][:password])
+          render json: { message: 'Password was successfully updated.' }, status: :ok
         else
           render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
         end
