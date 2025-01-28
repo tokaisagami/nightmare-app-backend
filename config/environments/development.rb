b3b5c52a13239f3df382fa9e98456b5691d9e524
tokaisagami <tokai.sagami.54@gmail.com>
2025-01-28 21:56:41 +0900

    add:default_url_options追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -69,4 +69,5 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
   config.action_mailer.delivery_method = :letter_opener_web
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3001 }
   end
