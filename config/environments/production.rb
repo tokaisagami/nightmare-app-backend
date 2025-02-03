e9ea87de34f251daf27a01145fd8ed7f37841068
tokaisagami <tokai.sagami.54@gmail.com>
2025-02-03 20:52:26 +0900

    update:Mailgun環境変数設定変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,11 +101,11 @@ Rails.application.configure do
 
   # SMTP settings for Mailgun
   config.action_mailer.smtp_settings = {
-    address: 'smtp.mailgun.org',
-    port: 587,
+    address: ENV['MAILGUN_SMTP_SERVER'],
+    port: ENV['MAILGUN_SMTP_PORT'],
     domain: ENV['MAILGUN_DOMAIN'],
-    user_name: 'postmaster@' + ENV['MAILGUN_DOMAIN'],
-    password: ENV['MAILGUN_API_KEY'],
+    user_name: ENV['MAILGUN_SMTP_LOGIN'],
+    password: ENV['MAILGUN_SMTP_PASSWORD'],
     authentication: 'plain',
     enable_starttls_auto: true
   }
